4b776f58308ec97838d589e6d3fc84b4a792af9a
ArtavazdGevorgyan <ag15012003@gmail.com>
2022-03-10 18:02:19 +0400

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,8 +1,24 @@
+#import os
 dct = dict()
 for index in range(0, 121):
-    dct[str(index)] = 0
+    dct[index] = 0
 
-with open("rawfile.txt") as file:
-    line = file.readline().split(" ")[:-1]
-    for i in line:
-        dct[i]+=1
+with open("rawfile.txt", "r") as file:
+    for line in file:
+        line = line.strip(' \n')
+        lst = list(line.split(" "))
+        for i in lst:
+            dct[int(i)] += 1
+file.close()
+
+
+file = open('sortedfile.txt', 'w')
+for i in dct.keys():
+    for j in range(0, dct[i]):
+        
+        file.write(str(i) + " ")
+
+#print(os.path.getsize("sortedfile.txt"))
+
+# Close the file
+file.close()
