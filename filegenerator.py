f272ad03468110acbbc3bc2fb35091feacbabb31
ArtavazdGevorgyan <ag15012003@gmail.com>
2022-03-11 10:08:14 +0400

    Update filegenerator.py

--- filegenerator.py
+++ filegenerator.py
@@ -1,15 +1,11 @@
 import random
 #import os
 
-# Tiv@ zbaxacnuma mijinum 3.17  byte taracq
-
-file = open("rawfile.txt", "w")
-for line in range(0, 40000000):
-    for i in range(0, 32):
-        file.write(str(random.randint(0, 120)) + " ")
-    file.write("\n")
-
-file.close()
+with open("rawfile.txt", "w") as file:
+    for line in range(0, 40000000):
+        for i in range(0, 32):
+            file.write(str(random.randint(0, 120)) + " ")
+        file.write("\n")
 
 #print(os.path.getsize("rawfile.txt"))
 
