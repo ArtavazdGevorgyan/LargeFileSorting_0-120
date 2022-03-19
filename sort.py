4ec318278ce472318e20d89546e5511cba3276bc
ArtavazdGevorgyan <ag15012003@gmail.com>
2022-03-19 10:58:06 +0400

    Update sort.py

--- sort.py
+++ sort.py
@@ -9,10 +9,13 @@ with open("rawfile.txt", "r") as file:
         lst = list(line.split(" "))
         for i in lst:
             dct[int(i)] += 1
-
+nl=0
 with open('sortedfile.txt', 'w') as file:
     for i in dct.keys():
         for j in range(0, dct[i]):
             file.write(str(i) + " ")
+            nl+=1
+            if nl%32==0:
+                file.write("\n")
 
 #print(os.path.getsize("sortedfile.txt"))
